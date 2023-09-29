dd131ec8e0f8dd3ba10daf9a4b4a5d1486a80849
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-09-29 00:38:43 -0600

    1st commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Invitaciones digitales
+Son invitaciones... y son digitales... jeje
+
+Son páginas web estáticas que sirven como invitaciones para eventos, así se pueden agregar animaciones o cosas bonitas.
---
+++ xv_hazel/resources/js/script.js
@@ -0,0 +1,6 @@
+let date = new Date('January 20, 2024 19:30:00')
+let now = new Date();
+let daysLeft = Math.ceil((date - now) / (1000 * 60 * 60 * 24));
+
+document.getElementById("countdown").innerText = daysLeft;
+
