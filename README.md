35923b473df880fd567b7dd98ad2b66c2fe5d134
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-10-12 17:34:13 -0600

    Agregue formulario y modifique contador

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Son invitaciones... y son digitales... jeje
 
 Son páginas web estáticas que sirven como invitaciones para eventos, así se pueden agregar animaciones o cosas bonitas.
+
+# Creditos
+## iconos
+### SmashIcons
+
+[<img src="xv_hazel/resources/icons/codigo-de-vestimenta.png" width="50px" height="auto">](https://www.flaticon.es/iconos-gratis/codigo-de-vestimenta)
+
--- xv_hazel/resources/js/index.js
+++ xv_hazel/resources/js/index.js
@@ -1,10 +1,28 @@
+let subMensaje1 = "Hola soy ";
+let subMensaje2 = ", confirmo mi asistencia a la fiesta de Emily";
+
 function onCountdownLoad() {
     let date = new Date('January 20, 2024 19:30:00');
     let now = new Date();
-    let daysLeft = Math.ceil((date - now) / (1000 * 60 * 60 * 24));
-    document.getElementById('countdown').innerText = daysLeft;
+    let distance = date - now
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById('days').innerText = days;
+    document.getElementById('hours').innerText = hours;
+    document.getElementById('minutes').innerText = minutes;
+    document.getElementById('seconds').innerText = seconds;
 }
 
-function main() {
-    onCountdownLoad();
+function onSubmit(event) {
+    event.preventDefault();
+    let name = document.getElementById("name").value;
+    let mensaje = (subMensaje1 + name + subMensaje2).replace(" ", "%20");
+    let url = "https://wa.me/+5215551817720?text=" + mensaje
+    window.location.href = url;
+}
+
+function onLoad() {
+    setInterval(onCountdownLoad, 1000);
 }
