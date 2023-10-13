e688f2266cd5d16fcdd9981772c3f9a5796de0eb
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2023-10-13 17:34:09 -0600

    Agregue responsive y musica

--- xv_hazel/resources/js/index.js
+++ xv_hazel/resources/js/index.js
@@ -1,8 +1,12 @@
 let subMensaje1 = "Hola soy ";
 let subMensaje2 = ", confirmo mi asistencia a la fiesta de Emily";
 
+const audio = new Audio("resources/audio/music.mp3");
+audio.loop = true;
+let isPlaying = false;
+
 function onCountdownLoad() {
-    let date = new Date('January 20, 2024 19:30:00');
+    let date = new Date('January 20, 2024 19:00:00');
     let now = new Date();
     let distance = date - now
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -23,6 +27,24 @@ function onSubmit(event) {
     window.location.href = url;
 }
 
+function playMusic() {
+    let playBtn = document.getElementById("play");
+    let stopBtn = document.getElementById("stop");
+    if(!isPlaying) {
+        playBtn.classList.remove("hidden");
+        stopBtn.classList.add("hidden");
+        audio.play();
+        isPlaying = true;
+    } else {
+        playBtn.classList.add("hidden");
+        stopBtn.classList.remove("hidden");
+        audio.pause();
+        audio.currentTime = 0;
+        isPlaying = false;
+    }
+}
+
 function onLoad() {
+    onCountdownLoad();
     setInterval(onCountdownLoad, 1000);
 }
