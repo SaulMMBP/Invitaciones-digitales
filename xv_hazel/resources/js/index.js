ecf2197ee64463f95d95628db806c708ff716926
Saul Malagon Martinez <sark_blues_friki95@hotmail.com>
2024-11-16 13:29:22 -0600

    Change index

--- xv_hazel/resources/js/index.js
+++ xv_hazel/resources/js/index.js
@@ -6,31 +6,31 @@ audio.loop = true;
 let isPlaying = false;
 
 function onCountdownLoad() {
-    let date = new Date('January 20, 2024 19:00:00');
+    let date = new Date("January 20, 2025 19:00:00");
     let now = new Date();
-    let distance = date - now
+    let distance = date - now;
     let days = Math.floor(distance / (1000 * 60 * 60 * 24));
     let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById('days').innerText = days;
-    document.getElementById('hours').innerText = hours;
-    document.getElementById('minutes').innerText = minutes;
-    document.getElementById('seconds').innerText = seconds;
+    document.getElementById("days").innerText = days;
+    document.getElementById("hours").innerText = hours;
+    document.getElementById("minutes").innerText = minutes;
+    document.getElementById("seconds").innerText = seconds;
 }
 
 function onSubmit(event) {
     event.preventDefault();
     let name = document.getElementById("name").value;
     let mensaje = (subMensaje1 + name + subMensaje2).replace(" ", "%20");
-    let url = "https://wa.me/+5215551817720?text=" + mensaje
+    let url = "https://wa.me/+5215551817720?text=" + mensaje;
     window.location.href = url;
 }
 
 function playMusic() {
     let playBtn = document.getElementById("play");
     let stopBtn = document.getElementById("stop");
-    if(!isPlaying) {
+    if (!isPlaying) {
         playBtn.classList.remove("hidden");
         stopBtn.classList.add("hidden");
         audio.play();
